9592a5fdb6aff710f8b8f23e1f5cbe4fa2e6c841
dog14230pp <dog14230pp@gmail.com>
2020-10-16 01:26:53 +0800

    Finish youtube API

--- lib/youtube_api.rb
+++ lib/youtube_api.rb
@@ -1,8 +1,57 @@
-#frozen_string_literal: true
+# frozen_string_literal: false
 
 require 'http'
+require 'json'
+require_relative 'comment'
 
 module CodePraise
-  #Library for Youtube Web API
+  # Library for Youtube Web API
   class YoutubeApi
-       
+    API_ROOT_URL = 'https://www.googleapis.com/youtube/v3'.freeze
+
+    # Define some errors
+    module Errors
+      class NotFound < StandardError; end
+      class Unauthorized < StandardError; end
+    end
+
+    HTTP_ERROR = {
+      401 => Errors::Unauthorized,
+      404 => Errors::NotFound
+    }.freeze
+
+    def initialize(key)
+      @yt_key = key
+    end
+
+    def get_comment(videoid, part = 'snippet', order = 'relevance')
+      url = "commentThreads?key=#{@yt_key}&videoId=#{videoid}"
+      url += "&order=#{order}&part=#{part}"
+      video_url = yt_api_path(url)
+      comment_data_raw = call_yt_url(video_url).parse
+      comment = Comment.new(comment_data_raw)
+      comment.extract
+    end
+
+    def get_reply(parent, part = 'snippet')
+      url = "comments?key=#{@yt_key}&parentId=#{parent}&part=#{part}"
+      reply_url = yt_api_path(url)
+      call_yt_url(reply_url).parse
+    end
+
+    private
+
+    def yt_api_path(path)
+      "#{API_ROOT_URL}/#{path}"
+    end
+
+    def call_yt_url(url)
+      result = HTTP.get(url)
+      successful?(result) ? result : raise(HTTP_ERROR[result.code])
+    end
+
+    def successful?(result)
+      HTTP_ERROR.keys.include?(result.code) ? false : true
+    end
+  end
+end
